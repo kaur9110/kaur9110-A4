40caf44aeb6b06819724be07145467d7c4d8f6d5
kaur9110 <81828935+kaur9110@users.noreply.github.com>
2021-04-16 14:12:14 -0400

    Add files via upload

---
+++ assets/data/classData.ts
@@ -0,0 +1,4 @@
+import {Kaur9110} from '../../app/kaur9110'
+export const studentInfo: Kaur9110 = {
+sid:991591402, sname:'Kirandeep Kaur', slogin:'kaur9110', scampus:'Davis Campus',atitle:'SYST24444 Assignment 4'
+};
